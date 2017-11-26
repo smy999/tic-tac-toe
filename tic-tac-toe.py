8985de9865e0017fc759fc16e7f05c214ad673e3
smy999 <33407191+smy999@users.noreply.github.com>
2017-11-26 22:57:14 +0900

    Update tic-tac-toe.py

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -15,6 +15,31 @@ def checked(i) :
       else :
             player = "X"
             button["bg"] = "lightgreen"
+            
+      if list[0]["text"] == list[1]["text"] == list[2]["text"] != "     ":
+            w_msg(player)
+      if list[3]["text"] == list[4]["text"] == list[5]["text"] != "     ":
+            w_msg(player)
+      if list[6]["text"] == list[7]["text"] == list[8]["text"] != "     ":
+            w_msg(player)
+      if list[0]["text"] == list[3]["text"] == list[6]["text"] != "     ":
+            w_msg(player)
+      if list[1]["text"] == list[4]["text"] == list[7]["text"] != "     ":
+            w_msg(player)
+      if list[2]["text"] == list[5]["text"] == list[8]["text"] != "     ":
+            w_msg(player)
+      if list[0]["text"] == list[4]["text"] == list[8]["text"] != "     ":
+            w_msg(player)
+      if list[2]["text"] == list[4]["text"] == list[6]["text"] != "     ":
+            w_msg(player)
+            
+def w_msg(player):
+      if player=="O":
+            m=Message(window, text="O is winner")
+            m.grid(row=3, column=0)
+      if player=="X":
+            m=Message(window, text="X is winner")
+            m.grid(row=3, column=0)
 
 window = Tk()
 player = "X"
